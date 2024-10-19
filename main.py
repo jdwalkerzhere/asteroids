33173e2ce848c0551fdbb7ddbd00ccd112c43fc8
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-10-19 15:20:38 -0400

    added player class and movement speed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main() -> None:
@@ -8,11 +9,14 @@ def main() -> None:
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2, radius=PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color='black')
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1_000
 
