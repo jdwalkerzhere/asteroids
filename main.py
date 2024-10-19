493bcca288d1261eb0e0a797e33eb0dc11e7efdc
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-10-19 14:14:55 -0400

    added circles and player radius

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 
 def main() -> None:
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main() -> None:
                 return
         screen.fill(color='black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1_000
 
 
 if __name__ == '__main__':
