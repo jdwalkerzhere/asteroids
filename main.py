17c251aa1f7c8cd0a44f73710a226b021827626b
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-10-19 11:59:07 -0400

    added constants.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 
+from constants import *
+
 
 def main() -> None:
     print('Starting asteroids!')
+    print(SCREEN_WIDTH)
+    print(SCREEN_HEIGHT)
 
 
 if __name__ == '__main__':
