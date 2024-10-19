503203def401351e1a4d7d7acea8d3286381fa9e
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-10-19 15:44:48 -0400

    Created container field on player and added to updatable and drawable PyGame Groups

--- main.py
+++ main.py
@@ -5,18 +5,39 @@ from player import Player
 
 
 def main() -> None:
+    # Initialize PyGame
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
+
+    # Time keeping
+    clock, dt = pygame.time.Clock(), 0
+
+    # Set the PyGame Screen constants
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2, radius=PLAYER_RADIUS)
+
+    # Make PyGame Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player = Player(
+        x=SCREEN_WIDTH/2,
+        y=SCREEN_HEIGHT/2,
+        radius=PLAYER_RADIUS,
+        containers=(updatable, drawable))
+
+    updatable.add(player)
+    drawable.add(player)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color='black')
-        player.update(dt)
-        player.draw(screen)
+        for sprite in updatable:
+            sprite.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1_000
 
